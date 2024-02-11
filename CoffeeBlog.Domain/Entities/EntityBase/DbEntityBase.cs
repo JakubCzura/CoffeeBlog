f707c21901e2dd9d6f06e47734cebf257eef409f
Jakub Czura <czurajakub@gmail.com>
2024-02-11 18:32:43 +0100

    Prepare base DB entity class

--- CoffeeBlog.Domain/Entities/EntityBase/DbEntityBase.cs
+++ CoffeeBlog.Domain/Entities/EntityBase/DbEntityBase.cs
@@ -1,6 +1,14 @@
-﻿namespace CoffeeBlog.Domain.Entities.EntityBase;
+﻿using System.ComponentModel.DataAnnotations;
 
+namespace CoffeeBlog.Domain.Entities.EntityBase;
+
+/// <summary>
+/// Represents a base class for database entities.
+/// <para>It contains the <see cref="Id"/> property, which is the primary key of the entity.</para>
+/// <para>It is abstract. All entities should inherit from this class</para>
+/// </summary>
 public abstract class DbEntityBase
 {
+    [Key]
     public int Id { get; set; }
 }
