980205c1f755634d75948808025dd220c4df50c6
Jakub Czura <czurajakub@gmail.com>
2024-02-15 00:08:21 +0100

    Services registration for layers

---
+++ CoffeeBlog.Infrastructure/ExtensionMethods/InfrastructureRegistration.cs
@@ -0,0 +1,35 @@
+﻿using CoffeeBlog.Application.Interfaces.Persistence.Repositories.Errors;
+using CoffeeBlog.Application.Interfaces.Persistence.Repositories.Requests;
+using CoffeeBlog.Application.Interfaces.Persistence.Repositories.Roles;
+using CoffeeBlog.Application.Interfaces.Persistence.Repositories.Users;
+using CoffeeBlog.Infrastructure.Persistence.DatabaseContext;
+using CoffeeBlog.Infrastructure.Persistence.Repositories.Errors;
+using CoffeeBlog.Infrastructure.Persistence.Repositories.Requests;
+using CoffeeBlog.Infrastructure.Persistence.Repositories.Roles;
+using CoffeeBlog.Infrastructure.Persistence.Repositories.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CoffeeBlog.Infrastructure.ExtensionMethods;
+
+public static class InfrastructureRegistration
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+                                                               IConfiguration configuration)
+    {
+        services.AddDbContext<CoffeeBlogDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("CoffeeBlogDbConnectionString"),
+                                 builer => builer.MigrationsAssembly(typeof(CoffeeBlogDbContext).Assembly.FullName));
+        });
+
+        services.AddScoped<IApiErrorRepository, ApiErrorRepository>();
+        services.AddScoped<IRequestDetailRepository, RequestDetailRepository>();
+        services.AddScoped<IRoleRepository, RoleRepository>();
+        services.AddScoped<IUserDetailRepository, UserDetailRepository>();
+        services.AddScoped<IUserRepository, UserRepository>();
+
+        return services;
+    }
+}
--- CoffeeBlog.Infrastructure/Persistence/DatabaseContext/CoffeeBlogDbContext.cs
+++ CoffeeBlog.Infrastructure/Persistence/DatabaseContext/CoffeeBlogDbContext.cs
@@ -1,7 +1,22 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CoffeeBlog.Domain.Entities.Errors;
+using CoffeeBlog.Domain.Entities.Requests;
+using CoffeeBlog.Domain.Entities.Roles;
+using CoffeeBlog.Domain.Entities.Users;
+using Microsoft.EntityFrameworkCore;
 
 namespace CoffeeBlog.Infrastructure.Persistence.DatabaseContext;
 
 public class CoffeeBlogDbContext(DbContextOptions<CoffeeBlogDbContext> options) : DbContext(options)
 {
+    public DbSet<ApiError> ApiErrors { get; set; }
+    public DbSet<RequestDetail> RequestDetails { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserDetail> UserDetails { get; set; }
+    public DbSet<UserToRole> UserToRoles { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
 }
