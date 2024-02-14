195165b2115e4cf20d9b664b8d562db21f0e498a
Jakub Czura <czurajakub@gmail.com>
2024-02-14 18:33:00 +0100

    Basic CRUD interface

---
+++ CoffeeBlog.Domain/Interfaces/Base/IDbEntityBaseRepository.cs
@@ -0,0 +1,20 @@
+﻿using CoffeeBlog.Domain.Entities.Base;
+
+namespace CoffeeBlog.Domain.Interfaces.Base;
+
+/// <summary>
+/// Generic interface to perform CRUD operations in database
+/// </summary>
+/// <typeparam name="T">Entity in database</typeparam>
+public interface IDbEntityBaseRepository<T> where T : DbEntityBase
+{
+    Task<int> CreateAsync(T entity);
+
+    Task<T?> GetAsync(int id);
+
+    Task<List<T>> GetAllAsync();
+
+    Task<int> UpdateAsync(T entity);
+
+    Task<int> DeleteAsync(int id);
+}
---
+++ CoffeeBlog.Domain/Interfaces/Requests/IRequestDetailRepository.cs
@@ -0,0 +1,8 @@
+﻿using CoffeeBlog.Domain.Entities.Requests;
+using CoffeeBlog.Domain.Interfaces.Base;
+
+namespace CoffeeBlog.Domain.Interfaces.Requests;
+
+public interface IRequestDetailRepository : IDbEntityBaseRepository<RequestDetail>
+{
+}
