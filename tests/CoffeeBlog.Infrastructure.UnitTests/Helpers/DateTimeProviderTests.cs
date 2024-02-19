e2033ff470519bf1be3ddac7eb83c1fa45fa421d
Jakub Czura <czurajakub@gmail.com>
2024-02-19 19:20:53 +0100

    Unit tests for DateTimeProvider

---
+++ tests/CoffeeBlog.Infrastructure.UnitTests/Helpers/DateTimeProviderTests.cs
@@ -0,0 +1,19 @@
+﻿using CoffeeBlog.Infrastructure.Helpers;
+using FluentAssertions;
+
+namespace CoffeeBlog.Infrastructure.UnitTests.Helpers;
+
+public class DateTimeProviderTests
+{
+    private readonly DateTimeProvider _dateTimeProvider = new();
+
+    [Fact]
+    public void UtcNow_should_ReturnCurrentUtcTime()
+        => _dateTimeProvider.UtcNow.Should()
+                                   .BeCloseTo(DateTime.UtcNow, new TimeSpan(0, 0, 0, 0, 1));
+
+    [Fact]
+    public void Now_should_ReturnCurrentTime()
+        => _dateTimeProvider.Now.Should()
+                                .BeCloseTo(DateTime.Now, new TimeSpan(0, 0, 0, 0, 1));
+}
