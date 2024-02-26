d4c8a2bd170d08c68add87b5b3d72bf5988a0839
Jakub Czura <czurajakub@gmail.com>
2024-02-26 18:39:48 +0100

    Configure RequestDetail entity

--- src/CoffeeBlog.Infrastructure/Persistence/EntitiesConfigurations/RequestDetailConfiguration.cs
+++ src/CoffeeBlog.Infrastructure/Persistence/EntitiesConfigurations/RequestDetailConfiguration.cs
@@ -8,6 +8,31 @@ public class RequestDetailConfiguration : IEntityTypeConfiguration<RequestDetail
 {
     public void Configure(EntityTypeBuilder<RequestDetailEntity> builder)
     {
-        throw new NotImplementedException();
+        builder.ToTable("RequestDetails");
+
+        builder.Property(x => x.ControllerName).IsRequired()
+                                               .HasMaxLength(100);
+
+        builder.Property(x => x.Path).IsRequired();
+
+        builder.Property(x => x.HttpMethod).IsRequired()
+                                           .HasMaxLength(20);
+
+        builder.Property(x => x.StatusCode).IsRequired();
+
+        builder.Property(x => x.RequestBody).IsRequired(false);
+
+        builder.Property(x => x.RequestContentType).IsRequired(false)
+                                                   .HasMaxLength(50);
+
+        builder.Property(x => x.ResponseBody).IsRequired(false);
+
+        builder.Property(x => x.ResponseContentType).IsRequired(false);
+
+        builder.Property(x => x.RequestTimeInMiliseconds).IsRequired();
+
+        builder.Property(x => x.SentAt).IsRequired();
+
+        builder.Property(x => x.UserId).IsRequired(false);
     }
 }
