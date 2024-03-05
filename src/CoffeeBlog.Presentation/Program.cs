9f06d2905d421ff04d8345d185a7a6ba7eb614e1
Jakub Czura <czurajakub@gmail.com>
2024-03-05 19:05:37 +0100

    Add swagger

--- src/CoffeeBlog.Presentation/Program.cs
+++ src/CoffeeBlog.Presentation/Program.cs
@@ -4,6 +4,7 @@ using CoffeeBlog.Infrastructure.ExtensionMethods;
 using Serilog;
 using Microsoft.AspNetCore.Mvc;
 using CoffeeBlog.Presentation.Middlewares;
+using Microsoft.OpenApi.Models;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,8 @@ builder.Services.AddInfrastructureDI(builder.Configuration);
 
 builder.Services.AddRazorComponents()
     .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddSwaggerGen(options => options.SwaggerDoc("v1", new OpenApiInfo { Title = "CoffeeBlog documentation", Version = "v1" }));
 
 builder.Host.UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration));
 builder.Services.AddControllers();
@@ -39,6 +42,8 @@ app.UseMiddleware<RequestDetailsMiddleware>();
 if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
+    app.UseSwagger();
+    app.UseSwaggerUI(options => options.SwaggerEndpoint("/swagger/v1/swagger.json", "Version 1.0"));
 }
 else
 {
