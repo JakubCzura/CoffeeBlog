0508775afee0d42fdf2e1b2002bc14b4d305c121
Jakub Czura <czurajakub@gmail.com>
2024-03-06 21:53:03 +0100

    Unit tests for EmailValidator

--- src/CoffeeBlog.Infrastructure/Migrations/20240306201815_Init.cs
+++ src/CoffeeBlog.Infrastructure/Migrations/20240306201815_Init.cs
@@ -3,205 +3,204 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace CoffeeBlog.Infrastructure.Migrations
+namespace CoffeeBlog.Infrastructure.Migrations;
+
+/// <inheritdoc />
+public partial class Init : Migration
 {
     /// <inheritdoc />
-    public partial class Init : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateTable(
-                name: "ApiError",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Exception = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Message = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_ApiError", x => x.Id);
-                });
+        migrationBuilder.CreateTable(
+            name: "ApiError",
+            columns: table => new
+            {
+                Id = table.Column<int>(type: "int", nullable: false)
+                    .Annotation("SqlServer:Identity", "1, 1"),
+                Exception = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                Message = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_ApiError", x => x.Id);
+            });
 
-            migrationBuilder.CreateTable(
-                name: "Role",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Role", x => x.Id);
-                });
+        migrationBuilder.CreateTable(
+            name: "Role",
+            columns: table => new
+            {
+                Id = table.Column<int>(type: "int", nullable: false)
+                    .Annotation("SqlServer:Identity", "1, 1"),
+                Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Role", x => x.Id);
+            });
 
-            migrationBuilder.CreateTable(
-                name: "User",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Username = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
-                    Email = table.Column<string>(type: "nvarchar(320)", maxLength: 320, nullable: false),
-                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_User", x => x.Id);
-                });
+        migrationBuilder.CreateTable(
+            name: "User",
+            columns: table => new
+            {
+                Id = table.Column<int>(type: "int", nullable: false)
+                    .Annotation("SqlServer:Identity", "1, 1"),
+                Username = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                Email = table.Column<string>(type: "nvarchar(320)", maxLength: 320, nullable: false),
+                Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_User", x => x.Id);
+            });
 
-            migrationBuilder.CreateTable(
-                name: "UserDetail",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    LastSuccessfullSignIn = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    LastFailedSignIn = table.Column<DateTime>(type: "datetime2", nullable: true),
-                    LastUsernameChange = table.Column<DateTime>(type: "datetime2", nullable: true),
-                    LastEmailChange = table.Column<DateTime>(type: "datetime2", nullable: true),
-                    LastPasswordChange = table.Column<DateTime>(type: "datetime2", nullable: true),
-                    UserId = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_UserDetail", x => x.Id);
-                });
+        migrationBuilder.CreateTable(
+            name: "UserDetail",
+            columns: table => new
+            {
+                Id = table.Column<int>(type: "int", nullable: false)
+                    .Annotation("SqlServer:Identity", "1, 1"),
+                CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                LastSuccessfullSignIn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                LastFailedSignIn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                LastUsernameChange = table.Column<DateTime>(type: "datetime2", nullable: true),
+                LastEmailChange = table.Column<DateTime>(type: "datetime2", nullable: true),
+                LastPasswordChange = table.Column<DateTime>(type: "datetime2", nullable: true),
+                UserId = table.Column<int>(type: "int", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_UserDetail", x => x.Id);
+            });
 
-            migrationBuilder.CreateTable(
-                name: "RequestDetail",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    ControllerName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
-                    Path = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    HttpMethod = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
-                    StatusCode = table.Column<int>(type: "int", nullable: false),
-                    RequestBody = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    RequestContentType = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
-                    ResponseBody = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    ResponseContentType = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    RequestTimeInMiliseconds = table.Column<long>(type: "bigint", nullable: false),
-                    SentAt = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    UserId = table.Column<int>(type: "int", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_RequestDetail", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_RequestDetail_User_UserId",
-                        column: x => x.UserId,
-                        principalTable: "User",
-                        principalColumn: "Id");
-                });
+        migrationBuilder.CreateTable(
+            name: "RequestDetail",
+            columns: table => new
+            {
+                Id = table.Column<int>(type: "int", nullable: false)
+                    .Annotation("SqlServer:Identity", "1, 1"),
+                ControllerName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                Path = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                HttpMethod = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                StatusCode = table.Column<int>(type: "int", nullable: false),
+                RequestBody = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                RequestContentType = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                ResponseBody = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                ResponseContentType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                RequestTimeInMiliseconds = table.Column<long>(type: "bigint", nullable: false),
+                SentAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                UserId = table.Column<int>(type: "int", nullable: true)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_RequestDetail", x => x.Id);
+                table.ForeignKey(
+                    name: "FK_RequestDetail_User_UserId",
+                    column: x => x.UserId,
+                    principalTable: "User",
+                    principalColumn: "Id");
+            });
 
-            migrationBuilder.CreateTable(
-                name: "UserLastCredential",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    LastPassword1 = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    LastPassword2 = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    LastPassword3 = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    UserId = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_UserLastCredential", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_UserLastCredential_User_UserId",
-                        column: x => x.UserId,
-                        principalTable: "User",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
+        migrationBuilder.CreateTable(
+            name: "UserLastCredential",
+            columns: table => new
+            {
+                Id = table.Column<int>(type: "int", nullable: false)
+                    .Annotation("SqlServer:Identity", "1, 1"),
+                LastPassword1 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                LastPassword2 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                LastPassword3 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                UserId = table.Column<int>(type: "int", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_UserLastCredential", x => x.Id);
+                table.ForeignKey(
+                    name: "FK_UserLastCredential_User_UserId",
+                    column: x => x.UserId,
+                    principalTable: "User",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade);
+            });
 
-            migrationBuilder.CreateTable(
-                name: "UserToRole",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    UserId = table.Column<int>(type: "int", nullable: false),
-                    RoleId = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_UserToRole", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_UserToRole_Role_RoleId",
-                        column: x => x.RoleId,
-                        principalTable: "Role",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_UserToRole_User_UserId",
-                        column: x => x.UserId,
-                        principalTable: "User",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
+        migrationBuilder.CreateTable(
+            name: "UserToRole",
+            columns: table => new
+            {
+                Id = table.Column<int>(type: "int", nullable: false)
+                    .Annotation("SqlServer:Identity", "1, 1"),
+                UserId = table.Column<int>(type: "int", nullable: false),
+                RoleId = table.Column<int>(type: "int", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_UserToRole", x => x.Id);
+                table.ForeignKey(
+                    name: "FK_UserToRole_Role_RoleId",
+                    column: x => x.RoleId,
+                    principalTable: "Role",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade);
+                table.ForeignKey(
+                    name: "FK_UserToRole_User_UserId",
+                    column: x => x.UserId,
+                    principalTable: "User",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade);
+            });
 
-            migrationBuilder.CreateIndex(
-                name: "IX_RequestDetail_UserId",
-                table: "RequestDetail",
-                column: "UserId");
+        migrationBuilder.CreateIndex(
+            name: "IX_RequestDetail_UserId",
+            table: "RequestDetail",
+            column: "UserId");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_User_Username_Email",
-                table: "User",
-                columns: new[] { "Username", "Email" },
-                unique: true);
+        migrationBuilder.CreateIndex(
+            name: "IX_User_Username_Email",
+            table: "User",
+            columns: new[] { "Username", "Email" },
+            unique: true);
 
-            migrationBuilder.CreateIndex(
-                name: "IX_UserLastCredential_UserId",
-                table: "UserLastCredential",
-                column: "UserId");
+        migrationBuilder.CreateIndex(
+            name: "IX_UserLastCredential_UserId",
+            table: "UserLastCredential",
+            column: "UserId");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_UserToRole_RoleId",
-                table: "UserToRole",
-                column: "RoleId");
+        migrationBuilder.CreateIndex(
+            name: "IX_UserToRole_RoleId",
+            table: "UserToRole",
+            column: "RoleId");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_UserToRole_UserId",
-                table: "UserToRole",
-                column: "UserId");
-        }
+        migrationBuilder.CreateIndex(
+            name: "IX_UserToRole_UserId",
+            table: "UserToRole",
+            column: "UserId");
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "ApiError");
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "ApiError");
 
-            migrationBuilder.DropTable(
-                name: "RequestDetail");
+        migrationBuilder.DropTable(
+            name: "RequestDetail");
 
-            migrationBuilder.DropTable(
-                name: "UserDetail");
+        migrationBuilder.DropTable(
+            name: "UserDetail");
 
-            migrationBuilder.DropTable(
-                name: "UserLastCredential");
+        migrationBuilder.DropTable(
+            name: "UserLastCredential");
 
-            migrationBuilder.DropTable(
-                name: "UserToRole");
+        migrationBuilder.DropTable(
+            name: "UserToRole");
 
-            migrationBuilder.DropTable(
-                name: "Role");
+        migrationBuilder.DropTable(
+            name: "Role");
 
-            migrationBuilder.DropTable(
-                name: "User");
-        }
+        migrationBuilder.DropTable(
+            name: "User");
     }
 }
---
+++ tests/CoffeeBlog.Application.UnitTests/Validators/SharedValidators/EmailValidatorTests.cs
@@ -0,0 +1,44 @@
+﻿using CoffeeBlog.Application.Validators.SharedValidators;
+using FluentAssertions;
+using FluentValidation.TestHelper;
+
+namespace CoffeeBlog.Application.UnitTests.Validators.SharedValidators;
+
+public class EmailValidatorTests
+{
+    private readonly EmailValidator _emailValidator = new();
+
+    [Fact]
+    public void Validate_should_Pass_when_EmailIsCorrect()
+        => _emailValidator.TestValidate("johny@emailprovider.com")
+                          .ShouldNotHaveAnyValidationErrors();
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData("s")]
+    [InlineData(" d")]
+    [InlineData("d s")]
+    [InlineData("dd s")]
+    [InlineData("@ s")]
+    [InlineData("@")]
+    [InlineData("@ ")]
+    [InlineData("@D")]
+    [InlineData("email.")]
+    [InlineData("email.com")]
+    [InlineData("email.@")]
+    public void Validate_should_Fail_when_EmailIsIncorrect(string email)
+        => _emailValidator.TestValidate(email)
+                          .ShouldHaveAnyValidationError();
+
+    [Fact]
+    public void Validate_should_Fail_when_EmailIsLongerThanMaxLengthAllowed()
+        => _emailValidator.TestValidate(new string('k', 321))
+                          .ShouldHaveAnyValidationError();
+
+    [Fact]
+    public void Validate_should_ThrowArgumentNullException_when_EmailIsNull()
+        => _emailValidator.Invoking(x => x.TestValidate(null!))
+                          .Should()
+                          .Throw<ArgumentNullException>();
+}
