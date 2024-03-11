4b16d39bae661b7fa5b657553997a00519f975b1
Jakub Czura <czurajakub@gmail.com>
2024-03-11 21:40:55 +0100

    Unit tests for StringExtensions

--- src/CoffeeBlog.Application/ExtensionMethods/String/StringExtensions.cs
+++ src/CoffeeBlog.Application/ExtensionMethods/String/StringExtensions.cs
@@ -2,6 +2,6 @@
 
 public static class StringExtensions
 {
-    public static string ToCamelCase(this string value)
-        => string.IsNullOrEmpty(value) ? value : char.ToLowerInvariant(value[0]) + value[1..];
+    public static string ToCamelCase(this string? value)
+        => string.IsNullOrWhiteSpace(value) ? string.Empty : char.ToLowerInvariant(value[0]) + value[1..];
 }
---
+++ tests/CoffeeBlog.Application.UnitTests/ExtensionMethods/String/StringExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿using CoffeeBlog.Application.ExtensionMethods.String;
+using FluentAssertions;
+
+namespace CoffeeBlog.Application.UnitTests.ExtensionMethods.String;
+
+public class StringExtensionsTests
+{
+    [InlineData("TestStringMessage", "testStringMessage")]
+    [InlineData("TestString", "testString")]
+    [InlineData("Test", "test")]
+    [InlineData("TesT", "tesT")]
+    [InlineData("test", "test")]
+    [Theory]
+    public void ToCamelCase_should_ReturnStringConvertedToCamelCase_when_StringIsSpecified(string value, string expected)
+        => value.ToCamelCase().Should().Be(expected);
+
+    [InlineData(null)]
+    [InlineData(" ")]
+    [InlineData("    ")]
+    [Theory]
+    public void ToCamelCase_should_ReturnEmptyString_when_StringIsNotSpecified(string? value)
+        => value.ToCamelCase().Should().Be(string.Empty);
+}
