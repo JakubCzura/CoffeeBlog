b49b94a9cc44faad3f3bdf288de0468ebece7020
Jakub Czura <czurajakub@gmail.com>
2024-04-10 18:33:22 +0200

    Init email stuff in NotificationProvider

---
+++ src/NotificationProvider/NotificationProvider.Domain/SettingsOptions/Email/EmailCoffeeBlogOptions.cs
@@ -0,0 +1,22 @@
+﻿namespace NotificationProvider.Domain.SettingsOptions.Email;
+
+/// <summary>
+/// Configuration options for e-mails sent by CoffeeBlog application based on appsettings.json.
+/// </summary>
+public class EmailCoffeeBlogOptions
+{
+    /// <summary>
+    /// Name of application, for example CoffeeBlog.
+    /// </summary>
+    public string SenderName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Email associated with CoffeeBlog application.
+    /// </summary>
+    public string Email { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Password for email associated with CoffeeBlog application.
+    /// </summary>
+    public string Password { get; set; } = string.Empty;
+}
---
+++ src/NotificationProvider/NotificationProvider.Domain/SettingsOptions/Email/EmailSmtpOptions.cs
@@ -0,0 +1,24 @@
+﻿using MailKit.Security;
+
+namespace NotificationProvider.Domain.SettingsOptions.Email;
+
+/// <summary>
+/// Configuration options for SMTP server.
+/// </summary>
+public class EmailSmtpOptions
+{
+    /// <summary>
+    /// SMTP server host.
+    /// </summary>
+    public string Host { get; set; } = string.Empty;
+
+    /// <summary>
+    /// SMTP server port.
+    /// </summary>
+    public int Port { get; set; }
+
+    /// <summary>
+    /// Encryption options for secure connection.
+    /// </summary>
+    public SecureSocketOptions SecureSocketOptions { get; set; } = SecureSocketOptions.StartTls;
+}
