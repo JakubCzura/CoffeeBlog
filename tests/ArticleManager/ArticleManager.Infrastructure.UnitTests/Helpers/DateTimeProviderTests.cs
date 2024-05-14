b283490fb1d14bc849e053051db46eaa2d27928c
Jakub Czura <czurajakub@gmail.com>
2024-05-14 21:51:25 +0200

    Add tests projects in solution

---
+++ tests/ArticleManager/ArticleManager.Infrastructure.UnitTests/Helpers/DateTimeProviderTests.cs
@@ -0,0 +1,19 @@
+﻿using ArticleManager.Infrastructure.Helpers;
+using FluentAssertions;
+
+namespace ArticleManager.Infrastructure.UnitTests.Helpers;
+
+public class DateTimeProviderTests
+{
+    private readonly DateTimeProvider _dateTimeProvider = new();
+
+    [Fact]
+    public void UtcNow_should_ReturnCurrentUtcTime()
+        => _dateTimeProvider.UtcNow.Should()
+                                   .BeCloseTo(DateTime.UtcNow, new TimeSpan(0, 0, 0, 0, 1));
+
+    [Fact]
+    public void Now_should_ReturnCurrentTime()
+        => _dateTimeProvider.Now.Should()
+                                .BeCloseTo(DateTime.Now, new TimeSpan(0, 0, 0, 0, 1));
+}
