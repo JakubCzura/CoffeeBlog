a31e9f8b4b3c9deda3f03174788f0b31890f3981
Jakub Czura <czurajakub@gmail.com>
2024-07-17 23:33:15 +0200

    PostManager: work at CreatePostCommandHandler

--- src/PostManager/PostManager.Application/Commands/Posts/CreatePost/CreatePostCommandHandler.cs
+++ src/PostManager/PostManager.Application/Commands/Posts/CreatePost/CreatePostCommandHandler.cs
@@ -1,11 +1,27 @@
 ﻿using MediatR;
+using PostManager.Application.Interfaces.Persistence.Repositories;
+using PostManager.Domain.Entities;
 
 namespace PostManager.Application.Commands.Posts.CreatePost;
 
-public class CreatePostCommandHandler : IRequestHandler<CreatePostCommand, int>
+public class CreatePostCommandHandler(IPostRepository _postRepository) 
+    : IRequestHandler<CreatePostCommand, int>
 {
-    public async Task<int> Handle(CreatePostCommand request, CancellationToken cancellationToken)
+    private readonly IPostRepository _postRepository = _postRepository;
+
+    public async Task<int> Handle(CreatePostCommand request, 
+                                  CancellationToken cancellationToken)
     {
+        //TODO: use automapper
+        Post post = new()
+        {
+            Title = request.Title,
+            Subtitle = request.Subtitle,
+            Content = request.Content,
+            UserId = request.UserId,
+        };
+
+        return await _postRepository.CreateAsync(post, cancellationToken);
         throw new NotImplementedException();
     }
 }
