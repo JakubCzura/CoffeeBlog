86f5e021b39ddf7a240fb0e9201be9cd9a15dcf4
Jakub Czura <czurajakub@gmail.com>
2024-07-21 15:48:59 +0200

    NotificationProvider: extension method to register db context

---
+++ src/NotificationProvider/NotificationProvider.Infrastructure/ExtensionMethods/Database/DbContextConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NotificationProvider.Domain.SettingsOptions.Database;
+using NotificationProvider.Infrastructure.Persistence.DatabaseContext;
+
+namespace NotificationProvider.Infrastructure.ExtensionMethods.Database;
+
+/// <summary>
+/// Configuration of database context.
+/// </summary>
+public static class DbContextConfiguration
+{
+    /// <summary>
+    /// Configures database context.
+    /// </summary>
+    /// <param name="services">Collection of dependency injection services.</param>
+    /// <param name="configuration">Appsettings.json</param>
+    /// <returns>Reference to <paramref name="services"/></returns>
+    public static IServiceCollection ConfigureDbContext(this IServiceCollection services,
+                                                        IConfiguration configuration)
+    {
+        DatabaseOptions databaseOptions = configuration.GetSection(DatabaseOptions.AppsettingsKey)
+                                                       .Get<DatabaseOptions>()!;
+
+        services.AddDbContext<NotificationProviderDbContext>(options =>
+            options.UseMongoDB(databaseOptions.ConnectionString, databaseOptions.DatabaseName));
+
+        return services;
+    }
+}
--- src/NotificationProvider/NotificationProvider.Infrastructure/ExtensionMethods/LayerRegistration/InfrastructureRegistration.cs
+++ src/NotificationProvider/NotificationProvider.Infrastructure/ExtensionMethods/LayerRegistration/InfrastructureRegistration.cs
@@ -1,13 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using NotificationProvider.Application.Interfaces.Email;
 using NotificationProvider.Application.Interfaces.Factories.Emails;
 using NotificationProvider.Application.Interfaces.Persistence.Repositories;
-using NotificationProvider.Domain.SettingsOptions.Database;
 using NotificationProvider.Infrastructure.Email;
+using NotificationProvider.Infrastructure.ExtensionMethods.Database;
 using NotificationProvider.Infrastructure.Factories.Emails;
-using NotificationProvider.Infrastructure.Persistence.DatabaseContext;
 using NotificationProvider.Infrastructure.Persistence.Repositories;
 
 namespace NotificationProvider.Infrastructure.ExtensionMethods.LayerRegistration;
@@ -17,11 +15,7 @@ public static class InfrastructureRegistration
     public static IServiceCollection AddInfrastructureDI(this IServiceCollection services,
                                                          IConfiguration configuration)
     {
-        DatabaseOptions databaseOptions = configuration.GetSection(DatabaseOptions.AppsettingsKey)
-                                                       .Get<DatabaseOptions>()!;
-
-        services.AddDbContext<NotificationProviderDbContext>(options =>
-            options.UseMongoDB(databaseOptions.ConnectionString, databaseOptions.DatabaseName));
+        services.ConfigureDbContext(configuration);
 
         services.AddScoped<IEmailMessageFactory, EmailMessageFactory>();
         services.AddScoped<IEmailServiceProvider, EmailServiceProvider>();
