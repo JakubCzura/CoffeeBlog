72d5c8a571c416342f324d4c9b311fc720f9775a
Jakub Czura <czurajakub@gmail.com>
2025-03-24 21:46:55 +0100

    Update to .NET9

--- tests/ArticleManager/ArticleManager.Infrastructure.UnitTests/Helpers/DateTimeProviderTests.cs
+++ tests/ArticleManager/ArticleManager.Infrastructure.UnitTests/Helpers/DateTimeProviderTests.cs
@@ -10,10 +10,10 @@ public class DateTimeProviderTests
     [Fact]
     public void UtcNow_should_ReturnCurrentUtcTime()
         => _dateTimeProvider.UtcNow.Should()
-                                   .BeCloseTo(DateTime.UtcNow, new TimeSpan(0, 0, 0, 0, 1));
+                                   .BeCloseTo(DateTime.UtcNow, new TimeSpan(0, 0, 0, 1, 0));
 
     [Fact]
     public void Now_should_ReturnCurrentTime()
         => _dateTimeProvider.Now.Should()
-                                .BeCloseTo(DateTime.Now, new TimeSpan(0, 0, 0, 0, 1));
+                                .BeCloseTo(DateTime.Now, new TimeSpan(0, 0, 0, 1, 0));
 }
