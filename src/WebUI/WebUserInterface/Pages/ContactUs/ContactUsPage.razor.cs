0c611261aed05e801a14b4e1011306a18832ee9c
Jakub Czura <czurajakub@gmail.com>
2025-04-30 16:04:01 +0200

    WebUserInterface: implement page to contact with CoffeeBlog staff

---
+++ src/WebUI/WebUserInterface/Pages/ContactUs/ContactUsPage.razor.cs
@@ -0,0 +1,36 @@
+﻿using MudBlazor;
+using Shared.Application.NotificationProvider.Commands.EmailMessages.ContactUs;
+
+namespace WebUserInterface.Pages.ContactUs;
+
+public partial class ContactUsPage
+{
+    private string resultMessage = string.Empty;
+    private bool isProcessing = false;
+
+    private MudForm contactUsForm;
+    private ContactUsCommand contactUsCommand = new();
+    private ContactUsCommandValidator contactUsCommandValidator = new();
+
+    private async Task Submit()
+    {
+        if (isProcessing)
+        {
+            return;
+        }
+
+        await contactUsForm.Validate();
+        if (!contactUsForm.IsValid)
+        {
+            return;
+        }
+
+        isProcessing = true;
+
+        // TODO: Implement logic when working on backend
+
+        resultMessage = "Thank you for reaching out! We'll get back to you soon.";
+
+        isProcessing = false;
+    }
+}
